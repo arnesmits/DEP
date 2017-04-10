3e8763552842413f8653b759a274656ea1d58b12
Arne Hindrik Smits <arne.smits@embl.de>
2017-04-10 17:14:50 +0200

    add units test

---
+++ tests/testthat/test_6_plot_functions1.R
@@ -0,0 +1,75 @@
+context("6 - Plot functions1")
+
+test_that("plot_single throws error without valid input", {
+  expect_error(plot_single("test_sign", "USP19", "centered"))
+  expect_error(plot_single(test_sign, USP19, "centered"))
+  expect_error(plot_single(test_sign, "USP19", centered))
+  expect_error(plot_single(test_sign, "USP19", "test"))
+  expect_error(plot_single(test_sign, "USP15", "centered"))
+  expect_error(plot_single(test_sign, "test", "centered"))
+
+  test_sign_error1 <- test_sign
+  SummarizedExperiment::colData(test_sign_error1) <- SummarizedExperiment::colData(test_sign_error1)[,-(3)]
+  expect_error(plot_single(test_sign_error1, "USP19", "centered"))
+
+  test_sign_error2 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error2) <- SummarizedExperiment::rowData(test_sign_error2)[,-(1)]
+  expect_error(plot_single(test_sign_error2, "USP19", "centered"))
+
+  test_sign_error3 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error3) <- SummarizedExperiment::rowData(test_sign_error3)[,-(26:31)]
+  expect_error(plot_single(test_sign_error3, "USP19", "centered"))
+})
+
+test_that("plot_single returns a ggplot object", {
+  expect_is(plot_single(test_sign, "USP19", "centered"), "ggplot")
+  expect_is(plot_single(test_sign, "USP19", "contrast"), "ggplot")
+})
+
+test_that("plot_heatmap throws error without valid input", {
+  expect_error(plot_heatmap("test_sign", "centered", k = 6, col_limit = 4, labelsize = 8))
+  expect_error(plot_heatmap(test_sign, centered, k = 6, col_limit = 4, labelsize = 8))
+  expect_error(plot_heatmap(test_sign, "centered", k = "6", col_limit = 4, labelsize = 8))
+  expect_error(plot_heatmap(test_sign, "centered", k = 6, col_limit = "4", labelsize = 8))
+  expect_error(plot_heatmap(test_sign, "centered", k = 6, col_limit = 4, labelsize = "8"))
+  expect_error(plot_heatmap(test_sign, "test", k = 6, col_limit = 4, labelsize = 8))
+
+  test_sign_error1 <- test_sign
+  SummarizedExperiment::colData(test_sign_error1) <- SummarizedExperiment::colData(test_sign_error1)[,-(3)]
+  expect_error(plot_heatmap(test_sign_error1, "centered", k = 6, col_limit = 4, labelsize = 8))
+
+  test_sign_error2 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error2) <- SummarizedExperiment::rowData(test_sign_error2)[,-(35)]
+  expect_error(plot_heatmap(test_sign_error2, "centered", k = 6, col_limit = 4, labelsize = 8))
+
+  test_sign_error3 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error3) <- SummarizedExperiment::rowData(test_sign_error3)[,-(26:28)]
+  expect_error(plot_heatmap(test_sign_error3, "centered", k = 6, col_limit = 4, labelsize = 8))
+})
+
+test_that("plot_heatmap returns a HeatmapList object", {
+  expect_is(plot_heatmap(test_sign, "centered", k = 6, col_limit = 4, labelsize = 3), "HeatmapList")
+  expect_is(plot_heatmap(test_sign, "contrast", k = 6, col_limit = 8, labelsize = 3), "HeatmapList")
+  expect_is(plot_heatmap(test_ibaq_sign, "contrast", k = 1, col_limit = 8, labelsize = 3), "HeatmapList")
+})
+
+test_that("plot_volcano throws error without valid input", {
+  expect_error(plot_volcano("test_sign", "Ubi6_vs_Ctrl", labelsize = 5, add_names = TRUE))
+  expect_error(plot_volcano(test_sign, Ubi6_vs_Ctrl, labelsize = 5, add_names = TRUE))
+  expect_error(plot_volcano(test_sign, "Ubi6_vs_Ctrl", labelsize = 5, add_names = "TRUE"))
+  expect_error(plot_volcano(test_sign, "Ubi6_vs_Ctrl", labelsize = "5", add_names = TRUE))
+  expect_error(plot_volcano(test_sign, "Ubi5_vs_Ctrl", labelsize = 5, add_names = TRUE))
+
+  test_sign_error1 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error1) <- SummarizedExperiment::rowData(test_sign_error1)[,-(1)]
+  expect_error(plot_volcano(test_sign_error1, "Ubi6_vs_Ctrl", labelsize = 5, add_names = TRUE))
+
+  test_sign_error2 <- test_sign
+  SummarizedExperiment::rowData(test_sign_error2) <- SummarizedExperiment::rowData(test_sign_error2)[,-(26:34)]
+  expect_error(plot_volcano(test_sign_error2, "Ubi6_vs_Ctrl", labelsize = 5, add_names = TRUE))
+})
+
+test_that("plot_volcano returns a ggplot object", {
+  expect_is(plot_volcano(test_sign, "Ubi6_vs_Ctrl", labelsize = 5, add_names = TRUE), "ggplot")
+  expect_is(plot_volcano(test_sign, "Ubi6_vs_Ctrl", add_names = FALSE), "ggplot")
+})
---
+++ tests/testthat/test_8_workflows.R
@@ -0,0 +1,100 @@
+context("8 - Workflows")
+
+test_that("LFQ throws error without valid input", {
+  expect_error(LFQ("test_data", UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, "UbiLength_ExpDesign", "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, MinProb, "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", Ctrl, "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", control, filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = test,
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = Gene.names, ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = Protein.IDs, alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = "0.05", lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = "1"))
+
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = "test",
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "Test", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.Names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.ids", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign[,-(2)], "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+  expect_error(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "test", filter = c("Reverse", "Potential.contaminant"),
+                   name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1))
+})
+
+test_that("LFQ returns a list object", {
+  expect_is(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1), "list")
+  expect_is(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05L, lfc = 1L), "list")
+  expect_is(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = "Reverse",
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1), "list")
+})
+
+test_that("LFQ returns a list object with all expected objects", {
+  expect_equal(names(LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1)), c("data","se","filt","norm","imputed","lm","sign","results","param"))
+})
+
+test_that("report throws error without valid input", {
+  res <- LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+             name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1)
+  expect_error(report("res"))
+  expect_error(report(res[-1]))
+})
+
+test_that("report output ", {
+  res <- LFQ(test_data, UbiLength_ExpDesign, "MinProb", "Ctrl", "control", filter = c("Reverse", "Potential.contaminant"),
+             name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1)
+  expect_output(report(res), "Report and results.txt file saved in:")
+})
+
+test_that("iBAQ throws error without valid input", {
+  result <- LFQ(test_ibaq[,-(31:32)], GFPip_ExpDesign, "MinProb", "WT", "control", filter = c("Reverse", "Contaminant"),
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1)
+  expect_error(iBAQ("result", test_ibaq_pep, "GFP_vs_WT", "Rbbp4"))
+  expect_error(iBAQ(result, "test_ibaq_pep", "GFP_vs_WT", "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep, GFP_vs_WT, "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep, "GFP_vs_WT", Rbbp4))
+  expect_error(iBAQ(result[-(1)], test_ibaq_pep, "GFP_vs_WT", "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep[,-(6)], "GFP_vs_WT", "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep[,-(14)], "GFP_vs_WT", "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep, "test", "Rbbp4"))
+  expect_error(iBAQ(result, test_ibaq_pep, "GFP_vs_WT", "test"))
+
+  result_error <- result
+  result_error$data <- result_error$data[,-(15:20)]
+  expect_error(iBAQ(result_error, test_ibaq_pep, "GFP_vs_WT", "Rbbp4"))
+
+  result_error2 <- result
+  rowData(result_error2$sign) <- rowData(result_error2$sign)[,-(1)]
+  expect_error(iBAQ(result_error2, test_ibaq_pep, "GFP_vs_WT", "Rbbp4"))
+
+  result_error3 <- result
+  rowData(result_error3$sign) <- rowData(result_error3$sign)[,-c(27:28,30)]
+  expect_error(iBAQ(result_error3, test_ibaq_pep, "GFP_vs_WT", "Rbbp4"))
+})
+
+test_that("iBAQ returns a data.frame", {
+  result <- LFQ(test_ibaq[,-(31:32)], GFPip_ExpDesign, "MinProb", "WT", "control", filter = c("Reverse", "Contaminant"),
+                name = "Gene.names", ids = "Protein.IDs", alpha = 0.05, lfc = 1)
+  expect_is(iBAQ(result, test_ibaq_pep, "GFP_vs_WT", "Rbbp4", level = 1), "data.frame")
+  expect_is(iBAQ(result, test_ibaq_pep, "GFP_vs_WT", "Rbbp4", level = 1L), "data.frame")
+})
+
