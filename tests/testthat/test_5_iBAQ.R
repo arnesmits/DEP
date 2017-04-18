8839bcedf4f8bee0b3c5ee444982a01da53d1cc4
Arne Smits <arne.smits@embl.de>
2017-04-18 15:33:38 +0200

    plot_ibaq tests

--- tests/testthat/test_5_iBAQ.R
+++ tests/testthat/test_5_iBAQ.R
@@ -17,7 +17,7 @@ test_that("merge_ibaq returns a data.frame", {
 test_that("merge_ibaq returns an object with the rigth dimensions and columns", {
   result <- merge_ibaq(test_ibaq, test_ibaq_pep)
   expect_equal(grep("iBAQ", colnames(result)), 4:9)
-  expect_equal(dim(result), c(359,9))
+  expect_equal(dim(result), c(359,10))
 })
 
 test_that("stoichiometry throws error without valid input", {
@@ -62,4 +62,28 @@ test_that("plot_stoi throws error without valid input", {
 test_that("plot_stoi returns a ggplot object", {
   expect_is(plot_stoi(test_stoi, 0.001), "ggplot")
   expect_is(plot_stoi(test_stoi, 0.001, 0.5), "ggplot")
+})
+
+test_that("plot_ibaq throws error without valid input", {
+  expect_error(plot_ibaq("test_ibaq_sign", "GFP_vs_WT", 3))
+  expect_error(plot_ibaq(test_ibaq_sign, GFP_vs_WT, 3))
+  expect_error(plot_ibaq(test_ibaq_sign, "GFP_vs_WT", "3"))
+  expect_error(plot_ibaq(test_ibaq_sign, "test", 3))
+
+  test_ibaq_sign_error1 <- test_ibaq_sign
+  SummarizedExperiment::rowData(test_ibaq_sign_error1) <- SummarizedExperiment::rowData(test_ibaq_sign_error1)[,-(1)]
+  expect_error(plot_ibaq(test_ibaq_sign_error1, "GFP_vs_WT", 3))
+
+  test_ibaq_sign_error2 <- test_ibaq_sign
+  SummarizedExperiment::rowData(test_ibaq_sign_error2) <- SummarizedExperiment::rowData(test_ibaq_sign_error2)[,-c(27,28,30)]
+  expect_error(plot_ibaq(test_ibaq_sign_error2, "GFP_vs_WT", 3))
+
+  test_ibaq_sign_error3 <- test_ibaq_sign
+  SummarizedExperiment::rowData(test_ibaq_sign_error3) <- SummarizedExperiment::rowData(test_ibaq_sign_error3)[,-(16:21)]
+  expect_error(plot_ibaq(test_ibaq_sign_error3, "GFP_vs_WT", 3))
+
+})
+
+test_that("plot_ibaq returns a ggplot object", {
+  expect_is(plot_ibaq(test_ibaq_sign, "GFP_vs_WT", 3), "ggplot")
 })
