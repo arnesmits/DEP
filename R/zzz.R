215d2c0b24bd9e6c5628c1d31b6b3c1516793b67
Arne Smits <arne.smits@embl.de>
2017-04-21 18:20:19 +0200

    add globalVeriables

---
+++ R/zzz.R
@@ -0,0 +1,24 @@
+.onLoad <- function(libname = find.package("DEP"), pkgname = "DEP"){
+
+  # CRAN Note avoidance
+  if(getRversion() >= "2.15.1")
+    utils::globalVariables(
+      c( # functions.R globalVariables
+        "ID", ".", "condition", "label",
+        "value", "rowname", "miss_val",
+        "samples", "logFC", "qval", "comparison",
+        "val", "name", "significant",
+
+        # iBAQ.R globalVariables
+        "Unique..Groups.", "Protein.group.IDs",
+        "mean_ctrl", "stoichiometry",
+
+        # plot_functions.R globalVariables
+        "bin", "Freq", "Var1", "missval", "num",
+        "cs", "cs_frac", "cluster", "index",
+        "iBAQ_value", "Peptides", "lfc", "ibaq",
+        "LFC", "ymin", "ymax", "x", "y", "z"
+        )
+    )
+  invisible()
+}
